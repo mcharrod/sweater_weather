664b803a28b7cfe24d6b3fceb4de3dc58ee43273
Katy Harrod <m.catherine.harrod@gmail.com>
2022-06-12 21:29:48 -0600

    Facade and controller action for forecast service

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,7 +1,10 @@
 class Api::V1::ForecastController < ApplicationController
   def index
     city = params[:location]
-    current_forecast = ForecastFacade.current_forecast(city)
+
+    all_weather = ForecastFacade.get_each_weather(city)
+
+    render json: Api::V1::ForecastSerializer.forecast_index(all_weather[0], all_weather[1], all_weather[2])
 
     # PhotoFacade.get_city_image(city)
   end
--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,7 +1,23 @@
 class ForecastFacade
-  def self.current_forecast(city)
-    weather = ForecastService.get_current_weather(city)
+  def self.get_each_weather(city)
+    all_weather = ForecastService.get_current_weather(city)
 
-    CurrentForecast.new(weather)
+    current = CurrentForecast.new(all_weather)
+
+    daily = all_weather[:daily][0..4].map do |daily_forecast|
+      DailyForecast.new(daily_forecast)
+    end
+
+    hourly = all_weather[:hourly][0..7].map do |hourly_forecast|
+      HourlyForecast.new(hourly_forecast)
+    end
+
+    [current, daily, hourly]
+  end
+
+  def get_hourly_weather(weather)
+  end
+
+  def get_daily_weather(weather)
   end
 end
