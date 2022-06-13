964c9588ec86ca3541ba6b18dff0daeff1de5368
Katy Harrod <m.catherine.harrod@gmail.com>
2022-06-13 16:19:21 -0600

    limit the number of poros built in the facade by the max limit that the user enters and count those results found

--- app/facades/books_facade.rb
+++ app/facades/books_facade.rb
@@ -2,12 +2,12 @@ class BooksFacade
   def self.search_books_by_city(city, limit)
     json = BooksService.search_for_books_by_city(city, limit)
     results_found = json[:numfound]
-    array = json[:docs].map do |book|
+    array = json[:docs][0..count(limit)].map do |book|
       Book.new(book)
     end
   end
 
-  # def self.count(user_input)
-  #   user_input.to_i - 1
-  # end
+  def self.count(user_input)
+    user_input.to_i - 1
+  end
 end
