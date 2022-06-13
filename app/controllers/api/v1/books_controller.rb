bbadc0a158ab5ac3df9ef937a99f7a755fa6e142
Katy Harrod <m.catherine.harrod@gmail.com>
2022-06-13 10:02:20 -0600

    Add simple guard statement for no quantity or negative quantity param

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::BooksController < ApplicationController
   def index
-    BooksFacade.search_books_by_city(params[:location], params[:quantity])
+    if params[:quantity].nil? || params[:quantity] < 1
+      params[:quantity] = 10
+      BooksFacade.search_books_by_city(params[:location], params[:quantity])
+    end
   end
 end
