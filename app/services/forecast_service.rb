a67a951a265b7c95ac82432ec9054992bfa05140
Katy Harrod <m.catherine.harrod@gmail.com>
2022-06-16 11:30:08 -0600

    Final product

--- app/services/forecast_service.rb
+++ app/services/forecast_service.rb
@@ -8,6 +8,6 @@ class ForecastService < BaseService
     faraday.params['lon'] = coordinates[:results][0][:locations][0][:latLng][:lng]
     faraday.params['units'] = 'imperial'
     end
-    x = JSON.parse(response.body, symbolize_names: true)
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
